2834b435907365f6a4c60025d74e565db5aa2876
Eli Sokeland <etsokeland@bsu.edu>
2019-10-13 13:43:53 -0400

    Updates

---
+++ precache-manifest.f2072f18a9650359f1fa4631b1790937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0125d1445b38864408ff7d7f07b181d",
+    "url": "/Eli017/eureka/index.html"
+  },
+  {
+    "revision": "95a4b8eda5e448439306",
+    "url": "/Eli017/eureka/static/css/main.26d62414.chunk.css"
+  },
+  {
+    "revision": "cec57bd104cf619aa1a1",
+    "url": "/Eli017/eureka/static/js/2.91a5adfb.chunk.js"
+  },
+  {
+    "revision": "95a4b8eda5e448439306",
+    "url": "/Eli017/eureka/static/js/main.dda5e77a.chunk.js"
+  },
+  {
+    "revision": "b09821a1ba9646d95b3e",
+    "url": "/Eli017/eureka/static/js/runtime-main.a0eef086.js"
+  },
+  {
+    "revision": "180579532a47690ff5ede47593efdf57",
+    "url": "/Eli017/eureka/static/media/Landing.18057953.jpg"
+  }
+]);
